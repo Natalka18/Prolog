db3d1240f7be933c5fc9a188a6c0e77ff4779480
Your Name <you@example.com>
2020-03-20 11:23:25 +0100

    zad2 z rekurencją ogonową

--- lista3/zadanie2.pl
+++ lista3/zadanie2.pl
@@ -1,11 +1,20 @@
-% max_sum_pom(L, Sum, Max) jest prawdziwy, jeśli sumą elementów pewnej podlisty
-% listy L oraz Max jest maksymalną taką sumą
-max_sum_pom([], 0, 0).
-max_sum_pom([X | L], Sum, Max) :-
-	max_sum_pom(L, S, M),
-	Sum_temp is S + X,
-	porownaj(Sum_temp, M, Max),
-	(Sum_temp < 0 -> Sum is 0; Sum is Sum_temp).
+% max_sum_pom(L, PrevSum, Sum, PrevMax, Max) jest prawdziwy, gdy:
+% - PrevSum jest sumą pewnej podlisty listy L bez pierwszego elementu,
+% - PrevMax jest największą wartością spośród sum po wszystkich możliwych
+% podlistach listy L bez pierwszego elementu,
+% - Max jest największą wartością spośród sum po wszystkich możliwych
+% podlistach listy L
+max_sum_pom([], PrevSum, PrevMax, Max) :-
+	porownaj(PrevSum, PrevMax, Max).
+max_sum_pom([X | L], PrevSum, PrevMax, Max) :-
+	Sum_temp is PrevSum + X,
+	porownaj(Sum_temp, PrevMax, NewPrevMax),
+	(Sum_temp < 0 -> NewPrevSum is 0; NewPrevSum is Sum_temp),
+	max_sum_pom(L, NewPrevSum, NewPrevMax, Max).
+	
+% max_sum(L, S)
+max_sum(L, S) :-
+	max_sum_pom(L, 0, 0, S).
 
 % porownaj(A, B, M) jest prawdziwy, jeśli M jest większym elementem z pary A, B
 % lub jeśli M = A w przypadku A = B
@@ -13,8 +22,3 @@ porownaj(A, B, B) :-
 	B > A.
 porownaj(A, B, A) :-
 	B =< A.
-
-% max_sum(L, S) jest prawdziwy, jeśli S jest największą wartością spośród sum
-% po wszystkich możliwych podlistach listy L
-max_sum(L, S) :-
-	max_sum_pom(L, _, S).
